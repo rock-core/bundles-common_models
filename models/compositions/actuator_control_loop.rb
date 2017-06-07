055184cead7b8ded056aca705374ba8e9b3afd07
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2017-06-07 15:11:04 -0300

    the Actuators type have been removed from base/types, remove corresponding models

--- models/compositions/actuator_control_loop.rb
+++
@@ -1,3 +0,0 @@
-require 'common_models/models/services/actuator_control_loop'
-require 'common_models/models/compositions/control_loop'
-Rock::Compositions::ControlLoop.declare 'Actuator'
--- models/services/actuator_control_loop.rb
+++
@@ -1,4 +0,0 @@
-require 'common_models/models/services/control_loop'
-import_types_from 'base'
-Rock::Services::ControlLoop.declare \
-    'Actuator', '/base/actuators/Command', '/base/actuators/Status'
