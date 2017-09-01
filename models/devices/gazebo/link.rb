001ef717dded4d8f8a3e66d659ec0e54da781bdf
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2017-09-01 15:05:08 -0300

    add missing requires

--- models/devices/gazebo/link.rb
+++ models/devices/gazebo/link.rb
@@ -1,4 +1,6 @@
 require 'common_models/models/devices/gazebo/entity'
+require 'common_models/models/services/pose'
+require 'common_models/models/services/velocity'
 require 'common_models/models/services/transformation'
 
 module CommonModels
@@ -9,11 +11,11 @@ module CommonModels
 
                 output_port 'link_state_samples', '/base/samples/RigidBodyState'
 
-                provides CommonModels::Services::Pose,
+                provides Services::Pose,
                     'pose_samples' => 'link_state_samples'
-                provides CommonModels::Services::Transformation,
+                provides Services::Transformation,
                     'transformation' => 'link_state_samples'
-                provides CommonModels::Services::Velocity,
+                provides Services::Velocity,
                     'velocity_samples' => 'link_state_samples'
             end
         end
