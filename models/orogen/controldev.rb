a3f8a3ebd3e880a223d16b032d3be6defda2ff4e
Alex Fernandes Neves <alexfneves@gmail.com>
2018-04-02 13:46:58 -0300

    Removing GraupnerMC20Task
    Currently the using_task_library 'controldev' throws an error because it doesn't recognize GraupnerMC20Task
    
    Signed-off-by: Alex Fernandes Neves <alexfneves@gmail.com>

--- models/orogen/controldev.rb
+++ models/orogen/controldev.rb
@@ -1,10 +1,6 @@
 require 'common_models/models/devices/input/joystick'
-require 'common_models/models/devices/input/graupner/mc20'
 
 class OroGen::Controldev::JoystickTask
     driver_for CommonModels::Devices::Input::Joystick, as: 'driver'
 end
 
-class OroGen::Controldev::GraupnerMC20Task
-    driver_for CommonModels::Devices::Input::Graupner::Mc20, as: 'driver'
-end
