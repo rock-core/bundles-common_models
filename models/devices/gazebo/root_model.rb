4043421c5977314359376f137a2913c6e950bfa5
Sylvain Joyeux <sylvain.joyeux@tidewise.io>
2025-08-19 17:12:05 -0300

    feat: add API to register and enumerate all submodels, links and joints defined in the system

--- models/devices/gazebo/root_model.rb
+++ models/devices/gazebo/root_model.rb
@@ -9,6 +9,40 @@ module CommonModels
             device_type "RootModel" do
                 provides Model
 
+                extend_device_configuration do
+                    def register_submodel(device)
+                        (@submodels ||= []) << device
+                    end
+
+                    def each_submodel(&block)
+                        (@submodels || []).each(&block)
+                    end
+
+                    def register_exported_joint(device)
+                        (@exported_joints ||= []) << device
+                    end
+
+                    def each_exported_joint(&block)
+                        (@exported_joints || []).each(&block)
+                    end
+
+                    def register_exported_link(device)
+                        (@exported_links ||= []) << device
+                    end
+
+                    def each_exported_link(&block)
+                        (@exported_links || []).each(&block)
+                    end
+
+                    def fully_instanciated_model
+                        ir = to_instance_requirements
+                        each_submodel { |m| ir = ir.merge(m.to_instance_requirements) }
+                        each_exported_link { |m| ir = ir.merge(m.to_instance_requirements) }
+                        each_exported_joint { |m| ir = ir.merge(m.to_instance_requirements) }
+                        ir
+                    end
+                end
+
                 input_port "model_pose", "/base/samples/RigidBodyState"
 
                 provides Services::Pose
